972a007ac160470562dadf1ee292069b2f03b9a8
caiobaraujo <caiobrega@gmail.com>
2022-08-09 23:51:35 -0300

    add new url in .env

--- src/helpers/OlxAPI.js
+++ src/helpers/OlxAPI.js
@@ -1,7 +1,7 @@
 import Cookies from "js-cookie";
 import qs from 'qs';
 
-const BASEAPI = 'http://localhost:5000';
+const BASEAPI = process.env.REACT_APP_API_URL;
 
 const apiFetchFile = async (endpoint, body ) => { 
     if(!body.token) {
